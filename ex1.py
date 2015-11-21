aadf5e055cf4635e1695110105bc88ea317cc214
pachap <omeramit@gmail.com>
2015-11-21 17:10:43 +0200

    OMER

--- ex1.py
+++ ex1.py
@@ -17,6 +17,8 @@ class BombermanProblem(search.Problem):
         that yields the successors one at a time, rather than building them
         all at once. Iterators will work fine within the framework."""
         return None
+
+OMER
     
     def h(self, node):
         """ This is the heuristic. It get a node (not a state)
